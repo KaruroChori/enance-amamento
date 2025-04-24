7539b4a21093e45a10ab3d2a4af88e55154f85d4
Karuro Chari <karurochori@users.noreply.github.com>
2025-04-24 07:39:00 +0100

    Update prototype load balancer.

--- src/experiments/test-dist-compute.cpp
+++ src/experiments/test-dist-compute.cpp
@@ -3,22 +3,34 @@
 #include <print>
 #include <chrono>
 
-auto slice_work = [](size_t device, float start, float end){
-    //TODO: This will fail on clang because of https://github.com/llvm/llvm-project/issues/136652
+#pragma omp requires unified_shared_memory
+
+
+//Just annoying workaround without lambdas to avoid https://github.com/llvm/llvm-project/issues/136652
+constexpr size_t HEIGHT=40000, WIDTH=80000;
+static uint8_t* data;
+
+auto slice_work(size_t device, float start, float end){
+    if(data==nullptr){
+        std::print("oh nooooo\n");
+        throw "NOOOO";
+    }
     #pragma omp target teams device(device) 
     {
 
-        /*#pragma omp distribute parallel for collapse(2) schedule(static,1)
-        for (int i = 0; i < render_height; i++) {
-            for (int j = 0; j < render_width; j++) {
-                vec2 coo = (vec2{j,i}*scale-0.5f*vec2{display_width,display_height})/(float)display_height;
-                layer_0[i*render_width+j]= scene.render(coo);
+        #pragma omp distribute parallel for collapse(2) schedule(static,1)
+        for (int i = (int)(HEIGHT*start); i < (int)(HEIGHT*end); i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                data[i*WIDTH+j]+=std::sqrt(i+j*i)+std::sqrt(j+j*i)+std::pow(j,i);
             }
-        }*/
+        }
     }
+
+    return;
 };
 
 int main(){
+    data= (uint8_t*) omp_alloc(HEIGHT*WIDTH);
     const size_t devnum = omp_get_num_devices();
     struct{
         float speed;
@@ -48,7 +60,9 @@ int main(){
         }
 
         float total_speed = 0;
-        #pragma omp parallel for reduction(+:total_speed)
+        //Cannot use this one. It would be nice to keep code more regular, but it requires OMP_WAIT_POLICY=passive not to be very wasteful. 
+        // So I will be using the std::async version I guess, since that policy cannot be set for each instance separately.
+        #pragma omp parallel for reduction(+:total_speed) schedule(dynamic,1)
         for(size_t i = 0; i<devnum ; i++){
             //std::print("{}\n",omp_get_thread_num());
             auto start = std::chrono::high_resolution_clock::now();
@@ -62,11 +76,79 @@ int main(){
         }
 
         //#pragma omp parallel for
-        for(size_t i = 1; i<devnum ; i++){
+        for(size_t i = 0; i<devnum ; i++){
             devices[i].weight=(1.f-lambda)*devices[i].weight+lambda*(devices[i].speed/total_speed);
+            printf("%ld %f\n",i, devices[i].weight);
         }
 
         //TODO: Implement memory constraints
     }
+
+    omp_free(data);
     return 0;
 }
+
+
+/*
+
+int main(){
+    data= (uint8_t*) omp_alloc(HEIGHT*WIDTH);
+    const size_t devnum = omp_get_num_devices();
+    struct{
+        float speed;
+        float weight;
+        float max_memory;
+    } devices[devnum];
+    float integral_weights[devnum];
+
+    float lambda = 0.1;
+
+    for(auto& device:devices)device={1.0f,1.0f/(float)devnum, INFINITY};
+
+
+    while(true){
+        std::future<float> futures[devnum];
+        //Setup the integral array to alway know absolute slices
+        if(devnum>0)integral_weights[0]=devices[0].weight;
+        for(size_t i = 1; i<devnum ; i++){
+            integral_weights[i]=devices[i].weight+integral_weights[i-1];
+        }
+
+        float total_speed = 0;
+        //#pragma omp parallel for reduction(+:total_speed) schedule(dynamic,1)
+        for(size_t i = 0; i<devnum ; i++){
+            futures[i] = std::async(std::launch::async,[&,i](){
+                auto start = std::chrono::high_resolution_clock::now();
+
+                slice_work(i,(i>0?integral_weights[i-1]:0),integral_weights[i]);
+    
+                auto end = std::chrono::high_resolution_clock::now();
+                auto tmp_speed = devices[i].weight/(end-start).count();
+                return tmp_speed;
+            });
+            //std::print("{}\n",omp_get_thread_num());
+        }
+
+        {
+            int w = 0;
+            for (auto &fut : futures) {
+                auto v = fut.get();
+                devices[w].speed=v;
+                total_speed+=v;
+                w++;
+            }
+        }
+        
+        //#pragma omp parallel for
+        for(size_t i = 0; i<devnum ; i++){
+            devices[i].weight=(1.f-lambda)*devices[i].weight+lambda*(devices[i].speed/total_speed);
+            printf("%ld %f\n",i, devices[i].weight);
+        }
+
+        //TODO: Implement memory constraints
+    }
+
+    omp_free(data);
+    return 0;
+}
+*/
