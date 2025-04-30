9156d3496a8f95dbe950abfb9da0d92589a3c99a
Karuro Chari <karurochori@users.noreply.github.com>
2025-04-30 23:56:10 +0100

    Progress with the XML parser, started work for the tree-builder

--- src/experiments/xml-parser.cpp
+++ src/experiments/xml-parser.cpp
@@ -3,9 +3,11 @@
 #include <cassert>
 #include <concepts>
 #include <cstddef>
+#include <functional>
 #include <print>
 #include <string_view>
 #include <expected>
+#include <vector>
 
 
 
@@ -86,16 +88,40 @@ struct base_t{
     constexpr std::expected<std::string_view,feature_t> name() const {return static_cast<const T*>(this)->name();}
     constexpr std::expected<std::string_view,feature_t> value() const {return static_cast<const T*>(this)->value();}
 
-    constexpr std::expected<std::pair<const unknown_t*, const unknown_t*>,feature_t> children() const {}
-    constexpr std::expected<std::pair<const attr_t*, const attr_t*>,feature_t> attrs() const {}
+    constexpr std::expected<std::pair<const unknown_t*, const unknown_t*>,feature_t> children() const {return static_cast<const T*>(this)->children();}
+    constexpr std::expected<std::pair<const attr_t*, const attr_t*>,feature_t> attrs() const {return static_cast<const T*>(this)->attrs();}
 
-    constexpr std::expected<const node_t*,feature_t> parent() const {}
-    constexpr std::expected<const unknown_t*,feature_t> prev() const {}
-    constexpr std::expected<const unknown_t*,feature_t> next() const {}
+    constexpr std::expected<const node_t*,feature_t> parent() const {return static_cast<const T*>(this)->parent();}
+    constexpr std::expected<const unknown_t*,feature_t> prev() const {return static_cast<const T*>(this)->prev();}
+    constexpr std::expected<const unknown_t*,feature_t> next() const {return static_cast<const T*>(this)->next();}
 };
 
 
-struct node_t : protected base_t<node_t>{
+struct attr_t : base_t<attr_t>{
+    private:
+    std::string_view _ns;
+    std::string_view _name;
+    std::string_view _value;
+
+    public:
+
+    using base_t::type;
+
+    constexpr static inline type_t deftype() {return type_t::ATTR;};
+    constexpr std::expected<std::string_view,feature_t> ns() const {return _ns;}
+    constexpr std::expected<std::string_view,feature_t> name() const {return _name;}
+    constexpr std::expected<std::string_view,feature_t> value() const {return _value;}
+
+    constexpr std::expected<std::pair<const unknown_t*, const unknown_t*>,feature_t> children() const {return std::unexpected(feature_t::NOT_SUPPORTED);}
+    constexpr std::expected<std::pair<const attr_t*, const attr_t*>,feature_t> attrs() const {return std::unexpected(feature_t::NOT_SUPPORTED);}
+
+    constexpr std::expected<const node_t*,feature_t> parent() const {return std::unexpected(feature_t::NOT_IMPLEMENTED);}
+    constexpr std::expected<const unknown_t*,feature_t> prev() const {return std::unexpected(feature_t::NOT_IMPLEMENTED);}
+    constexpr std::expected<const unknown_t*,feature_t> next() const {return std::unexpected(feature_t::NOT_IMPLEMENTED);}
+};
+
+
+struct node_t : base_t<node_t>{
     private:
     std::string_view _ns;
     std::string_view _name;
@@ -113,30 +139,34 @@ struct node_t : protected base_t<node_t>{
 
     using base_t::type;
     
+    node_t(std::string_view ns, std::string_view name):_ns(ns),_name(name){}
+
     constexpr static inline type_t deftype() {return type_t::NODE;};
     constexpr std::expected<std::string_view,feature_t> ns() const {return _ns;}
     constexpr std::expected<std::string_view,feature_t> name() const {return _name;}
     constexpr std::expected<std::string_view,feature_t> value() const {return std::unexpected(feature_t::NOT_SUPPORTED);}
+
+    constexpr std::expected<std::pair<const unknown_t*, const unknown_t*>,feature_t> children() const {
+        return std::pair{
+            (const unknown_t*)((const uint8_t*)this+_children_begin),
+            (const unknown_t*)((const uint8_t*)this+_children_end)
+        };
+    }
+    constexpr std::expected<std::pair<const attr_t*, const attr_t*>,feature_t> attrs() const {
+        return std::pair{
+            (const attr_t*)((const uint8_t*)this+sizeof(node_t)),
+            (const attr_t*)((const uint8_t*)this+sizeof(node_t)+sizeof(attr_t)*attrs_count)
+        };
+    }
+
+    constexpr std::expected<const node_t*,feature_t> parent() const {return (const node_t*)((const uint8_t*)this+_parent);}
+    constexpr std::expected<const unknown_t*,feature_t> prev() const {return (const unknown_t*)((const uint8_t*)this+_prev);}
+    constexpr std::expected<const unknown_t*,feature_t> next() const {return (const unknown_t*)((const uint8_t*)this+_next);}
 };
 
-struct attr_t : protected base_t<attr_t>{
-    private:
-    std::string_view _ns;
-    std::string_view _name;
-    std::string_view _value;
-
-    public:
-
-    using base_t::type;
-
-    constexpr static inline type_t deftype() {return type_t::ATTR;};
-    constexpr std::expected<std::string_view,feature_t> ns() const {return _ns;}
-    constexpr std::expected<std::string_view,feature_t> name() const {return _name;}
-    constexpr std::expected<std::string_view,feature_t> value() const {return _value;}
-};
 
 template<typename T>
-struct leaf_t : protected base_t<T>{
+struct leaf_t : base_t<T>{
     private:
     std::string_view _value;
     delta_ptr_t _parent;
@@ -148,21 +178,37 @@ struct leaf_t : protected base_t<T>{
     using base_t<T>::type;
     leaf_t() = delete;
 
-    constexpr static inline type_t deftype() {return T::deftype();};
     constexpr std::expected<std::string_view,feature_t> ns() const {return std::unexpected(feature_t::NOT_SUPPORTED);}
     constexpr std::expected<std::string_view,feature_t> name() const {return std::unexpected(feature_t::NOT_SUPPORTED);}
     constexpr std::expected<std::string_view,feature_t> value() const {return _value;}
+
+    constexpr std::expected<std::pair<const unknown_t*, const unknown_t*>,feature_t> children() const {return std::unexpected(feature_t::NOT_SUPPORTED);}
+    constexpr std::expected<std::pair<const attr_t*, const attr_t*>,feature_t> attrs() const {return std::unexpected(feature_t::NOT_SUPPORTED);}
+
+    constexpr std::expected<const node_t*,feature_t> parent() const {return (const node_t*)((const uint8_t*)this+_parent);}
+    constexpr std::expected<const unknown_t*,feature_t> prev() const {return (const unknown_t*)((const uint8_t*)this+_prev);}
+    constexpr std::expected<const unknown_t*,feature_t> next() const {return (const unknown_t*)((const uint8_t*)this+sizeof(leaf_t));}
 };
 
-struct comment_t : leaf_t<comment_t>{};
+struct comment_t : leaf_t<comment_t>{
+    constexpr static inline type_t deftype() {return type_t::COMMENT;};
+};
 
-struct cdata_t : leaf_t<cdata_t>{};
+struct cdata_t : leaf_t<cdata_t>{
+    constexpr static inline type_t deftype() {return type_t::CDATA;};
+};
 
-struct text_t : leaf_t<text_t>{};
+struct text_t : leaf_t<text_t>{
+    constexpr static inline type_t deftype() {return type_t::TEXT;};
+};
 
-struct proc_t : leaf_t<proc_t>{};
+struct proc_t : leaf_t<proc_t>{
+    constexpr static inline type_t deftype() {return type_t::PROC;};
+};
 
-struct inject_t : leaf_t<inject_t>{};
+struct inject_t : leaf_t<inject_t>{
+    constexpr static inline type_t deftype() {return type_t::INJECT;};
+};
 
 static_assert(thing_i<node_t>);
 static_assert(thing_i<attr_t>);
@@ -191,6 +237,13 @@ struct unknown_t : base_t<unknown_t>{
     constexpr std::expected<std::string_view,feature_t> ns() const {DISPATCH(ns(),std::terminate());}
     constexpr std::expected<std::string_view,feature_t> name() const {DISPATCH(name(),std::terminate());}
     constexpr std::expected<std::string_view,feature_t> value() const {DISPATCH(value(),std::terminate());}
+
+    constexpr std::expected<std::pair<const unknown_t*, const unknown_t*>,feature_t> children() const {DISPATCH(children(),std::terminate());}
+    constexpr std::expected<std::pair<const attr_t*, const attr_t*>,feature_t> attrs() const {DISPATCH(attrs(),std::terminate());}
+
+    constexpr std::expected<const node_t*,feature_t> parent() const {DISPATCH(parent(),std::terminate());}
+    constexpr std::expected<const unknown_t*,feature_t> prev() const {DISPATCH(prev(),std::terminate());}
+    constexpr std::expected<const unknown_t*,feature_t> next() const {DISPATCH(next(),std::terminate());}
 };
 
 
@@ -198,6 +251,74 @@ static_assert(thing_i<unknown_t>);
 
 
 #undef DISPATCH
+
+struct tree{
+    node_t root;
+    std::vector<uint8_t> buffer;
+
+    /**
+     * @brief Reorder (in-place) children of a node based on a custom ordering criterion.
+     * 
+     * @param ref Node whose children are to be ordered
+     * @param fn Criterion to determine the order of nodes. -1 for left<right, 0 for equals, 1 for left>right
+     * @return true if the operation is successful
+     */
+    bool reorder_children(const node_t* ref, const std::function<int(const unknown_t*, const unknown_t*)>& fn){}
+
+    /**
+     * @brief Reorder (in-place) attributes of a node based on a custom ordering criterion.
+     * 
+     * @param ref Node whose attributes are to be ordered
+     * @param fn Criterion to determine the order of nodes. -1 for left<right, 0 for equals, 1 for left>right
+     * @return true if the operation is successful
+     */
+    bool reorder_attrs(const node_t* ref, const std::function<int(const attr_t*, const attr_t*)>& fn){}
+
+    /**
+     * @brief 
+     * 
+     * @param ref The node/leaf where to expand.
+     * @param start 
+     * @param end 
+     * @return true 
+     * @return false 
+     */
+    bool inject(const unknown_t* ref, const unknown_t* start, const unknown_t* end);
+
+    bool inject_before(const unknown_t* ref, const unknown_t* start, const unknown_t* end);
+    bool inject_after(const unknown_t* ref, const unknown_t* start, const unknown_t* end);
+
+    /**
+     * @brief Return a perfect deep copy of the current tree structure.
+     * 
+     * @return tree 
+     */
+    tree clone() const;
+};
+
+struct builder{
+    private:
+    std::vector<uint8_t> buffer;
+    bool open = true;
+    bool attribute_block = false;   //True after a begin to add attributes. It is automatically closed when any other command is triggered.
+
+    public:
+    bool begin(std::string_view name, std::string_view ns=""){
+        //buffer.emplace_back(node_t{ns,name})
+    }
+
+    bool end(){}
+
+    bool attribute(std::string_view name, std::string_view value, std::string_view ns=""){}
+
+    bool text(std::string_view value);
+    bool comment(std::string_view value){}
+    bool cdata(std::string_view value){}
+    bool proc(std::string_view value){}
+    bool inject(std::string_view value){}
+
+    bool close(){open=false;}
+};
 
 }
 
