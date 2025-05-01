c169699ba450d89cc8ade3166dd059052f0d36bf
Karuro Chari <karurochori@users.noreply.github.com>
2025-05-01 22:18:07 +0100

    Ready to move the XML parser in its own repo. It now supports all XML things, like cdata comments etc. It also has functions for serialization and to validate that labels are well-formed. And it serialize into valid xml on ostream :).

--- src/experiments/xml-parser.cpp
+++ src/experiments/xml-parser.cpp
@@ -17,8 +17,10 @@
 #include <iterator>
 #include <print>
 #include <stack>
+#include <string>
 #include <string_view>
 #include <expected>
+#include <variant>
 #include <vector>
 
 
@@ -72,6 +74,125 @@ enum struct type_t{
     INJECT
 };
 
+namespace serialize{
+    //https://stackoverflow.com/questions/1091945/what-characters-do-i-need-to-escape-in-xml-documents
+    //An attempt will be made to keep the input string_view, unless escaping is needed.
+    
+    typedef std::optional<std::variant<std::string,std::string_view>> ret_t;
+
+    constexpr std::optional<std::string_view> validate_xml_label(std::string_view str){
+        int pos = 0;
+        for(auto& c : str){
+            if(pos==0 && (c=='_' or (c>'a' && c<'z') or (c>'A' && c<'Z'))){/*OK*/}
+            //In theory some intervals of utf8 should be negated. But this filter is good enough for now.
+            else if(pos!=0 && (c=='_' or c=='.' or c=='-' or (c>'0' && c<'9') or (c>'a' && c<'z') or (c>'A' && c<'Z') or (c>127))){/*OK*/}
+            else return {};
+            pos++;
+        }
+        return str;
+    }
+
+    constexpr ret_t to_xml_attr_1(std::string_view str){
+        int rule_a = 0;
+        for(auto& c : str){
+            if(c=='<')rule_a+=sizeof("&lt;")-1-1;
+            else if(c=='&')rule_a+=sizeof("&amp;")-1-1;
+            else if(c=='\'')rule_a+=sizeof("&apos;")-1-1;
+            //Maybe some config to enable the others as well even if not needed for a correct XML serialization?
+        }
+        if(rule_a==0)return str;
+        else{
+            std::string tmp;
+            tmp.reserve(str.length()+rule_a);
+            for(auto& c : str){
+                if(c=='<')tmp+="&lt;";
+                else if(c=='&')tmp+="&amp;";
+                else if(c=='\'')tmp+="&apos;";
+                else tmp+=c;
+            }
+            return tmp;
+        }
+    }
+
+    constexpr ret_t to_xml_attr_2(std::string_view str){
+        int rule_a = 0;
+        for(auto& c : str){
+            if(c=='<')rule_a+=sizeof("&lt;")-1-1;
+            else if(c=='&')rule_a+=sizeof("&amp;")-1-1;
+            else if(c=='"')rule_a+=sizeof("&quot;")-1-1;
+            //Maybe some config to enable the others as well even if not needed for a correct XML serialization?
+        }
+        if(rule_a==0)return str;
+        else{
+            std::string tmp;
+            tmp.reserve(str.length()+rule_a);
+            for(auto& c : str){
+                if(c=='<')tmp+="&lt;";
+                else if(c=='&')tmp+="&amp;";
+                else if(c=='"')tmp+="&quot;";
+                else tmp+=c;
+            }
+            return tmp;
+        }
+    }
+
+    constexpr ret_t to_xml_text(std::string_view str){
+        int rule_a = 0;
+        int rule_b = 0;
+        for(auto& c : str){
+            if(c=='<')rule_a+=sizeof("&lt;")-1-1;
+            else if(c=='&')rule_a+=sizeof("&amp;")-1-1;
+            if(rule_b==0 && c==']')rule_b=1;
+            if(rule_b==1 && c==']')rule_b=2;
+            if(rule_b==2 && c=='>')return {};   //]]> not allowed in text, it must be escaped.
+
+            //Maybe some config to enable the others as well even if not needed for a correct XML serialization?
+        }
+        if(rule_a==0)return str;
+        else{
+            std::string tmp;
+            tmp.reserve(str.length()+rule_a);
+            for(auto& c : str){
+                if(c=='<')tmp+="&lt;";
+                else if(c=='&')tmp+="&amp;";
+                else tmp+=c;
+            }
+            return tmp;
+        }
+    }
+
+    constexpr ret_t to_xml_cdata(std::string_view str){
+        int rule_a = 0;
+        for(auto& c : str){
+            if(rule_a==0 && c==']')rule_a=1;
+            else if(rule_a==1 && c==']')rule_a=2;
+            else if(rule_a==2 && c=='>')return {};   //Disallowed sequence ]]>
+            else rule_a=0;
+        }
+        return str;
+    }
+
+    constexpr ret_t to_xml_comment(std::string_view str){
+        int rule_a = 0;
+        for(auto& c : str){
+            if(rule_a==0 && c=='-')rule_a=1;
+            else if(rule_a==1 && c=='-')return {};   //Disallowed sequence --
+            else rule_a=0;
+        }
+        return str;
+    }
+
+    constexpr ret_t to_xml_proc(std::string_view str){
+        int rule_a = 0;
+        for(auto& c : str){
+            if(rule_a==0 && c=='?')rule_a=1;
+            else if(rule_a==1 && c=='>')return {};   //Disallowed sequence ?>
+            else rule_a=0;
+        }
+        return str;
+    }
+}
+
 template<typename T>
 concept thing_i = requires(T self){
     {self.type()} -> std::same_as<type_t>;
@@ -89,6 +210,8 @@ concept thing_i = requires(T self){
     {self.has_parent()} -> std::same_as<bool>;
     {self.has_prev()} -> std::same_as<bool>;
     {self.has_next()} -> std::same_as<bool>;
+
+    {self.path()} -> std::same_as<std::string>;
 };
 
 template <typename T>
@@ -116,8 +239,12 @@ struct base_t{
     constexpr inline bool has_prev() const {return static_cast<const T*>(this)->has_prev();}
     constexpr inline bool has_next() const {return static_cast<const T*>(this)->has_next();}
 
-    auto children_fwd() const;
-    auto attrs_fwd() const;
+    constexpr inline std::string path() const {
+        return std::format("{}/{}",parent()!=nullptr?parent()->path():"",static_cast<const T*>(this)->path_h());
+    }
+
+    constexpr auto children_fwd() const;
+    constexpr auto attrs_fwd() const;
 
     friend Builder;
     friend Tree;
@@ -132,11 +259,15 @@ struct attr_t{
 
     public:
 
-    constexpr inline attr_t(std::string_view _ns, std::string_view _name, std::string_view _value):_ns(_ns),_name(_name),_value(_value){}
+    constexpr inline attr_t(std::string_view _ns, std::string_view _name, std::string_view _value):
+        _ns(*serialize::validate_xml_label(_ns)),
+        _name(*serialize::validate_xml_label(_name)),
+        _value(_value){}
 
     constexpr inline std::expected<std::string_view,feature_t> ns() const {return _ns;}
     constexpr inline std::expected<std::string_view,feature_t> name() const {return _name;}
     constexpr inline std::expected<std::string_view,feature_t> value() const {return _value;}
+    
 };
 
 
@@ -153,7 +284,11 @@ struct node_t : base_t<node_t>{
     std::string_view _ns;
     std::string_view _name;
 
-    constexpr inline node_t(node_t* _parent, std::string_view _ns, std::string_view _name):_parent((uint8_t*)_parent-(uint8_t*)this),_ns(_ns),_name(_name){
+    constexpr inline node_t(node_t* _parent, std::string_view _ns, std::string_view _name):
+        _parent((uint8_t*)_parent-(uint8_t*)this),
+        _ns(*serialize::validate_xml_label(_ns)),
+        _name(*serialize::validate_xml_label(_name))
+    {
         _size=0;
         attrs_count=0;
     }
@@ -201,6 +336,10 @@ struct node_t : base_t<node_t>{
     constexpr inline bool has_prev() const {return _prev!=0;}
     constexpr inline bool has_next() const {return _next!=0;}
 
+    constexpr inline std::string path_h() const {
+        return std::format("{}{}{}", _ns, _ns==""?"":":", _name);
+    }
+    
     friend Builder;
     friend Tree;
     friend unknown_t;
@@ -219,11 +358,15 @@ struct leaf_t : base_t<T>{
     constexpr inline void set_parent(node_t* parent){_parent=(uint8_t*)parent-(uint8_t*)this;}
     constexpr inline void set_prev(unknown_t* prev){_prev=(uint8_t*)prev-(uint8_t*)this;}
     constexpr inline void set_next(unknown_t* next){/*_next=(uint8_t*)next-(uint8_t*)this;*/}
+
+
+    protected:
     
+    constexpr leaf_t(std::string_view value):_value(value){}
+
     public:
 
     using base_t<T>::type;
-    leaf_t() = delete;
 
     constexpr inline std::expected<std::string_view,feature_t> ns() const {return std::unexpected(feature_t::NOT_SUPPORTED);}
     constexpr inline std::expected<std::string_view,feature_t> name() const {return std::unexpected(feature_t::NOT_SUPPORTED);}
@@ -246,35 +389,50 @@ struct leaf_t : base_t<T>{
 };
 
 struct comment_t : leaf_t<comment_t>{
+    comment_t(std::string_view value):leaf_t(value){}
     constexpr static inline type_t deftype() {return type_t::COMMENT;};
+
+    constexpr inline std::string path_h() const { return std::format("#comment"); }
 
     friend Builder;
     friend Tree;
 };
 
 struct cdata_t : leaf_t<cdata_t>{
+    cdata_t(std::string_view value):leaf_t(value){}
     constexpr static inline type_t deftype() {return type_t::CDATA;};
+
+    constexpr inline std::string path_h() const { return std::format("#cdata"); }
 
     friend Builder;
     friend Tree;
 };
 
 struct text_t : leaf_t<text_t>{
+    text_t(std::string_view value):leaf_t(value){}
     constexpr static inline type_t deftype() {return type_t::TEXT;};
 
+    constexpr inline std::string path_h() const { return std::format("#text"); }
+    
     friend Builder;
     friend Tree;
 };
 
 struct proc_t : leaf_t<proc_t>{
+    proc_t(std::string_view value):leaf_t(value){}
     constexpr static inline type_t deftype() {return type_t::PROC;};
+
+    constexpr inline std::string path_h() const { return std::format("#proc"); }
 
     friend Builder;
     friend Tree;
 };
 
 struct inject_t : leaf_t<inject_t>{
+    inject_t(std::string_view value):leaf_t(value){}
     constexpr static inline type_t deftype() {return type_t::INJECT;};
+
+    constexpr inline std::string path_h() const { return std::format("#leaf"); }
 
     friend Builder;
     friend Tree;
@@ -374,7 +532,6 @@ struct node_iterator{
     pointer m_ptr;
 };
 
-//TODO: implement
 struct attr_iterator{
     using iterator_category = std::bidirectional_iterator_tag;
     using difference_type   = std::ptrdiff_t;
@@ -403,7 +560,7 @@ struct attr_iterator{
 };
 
 template <typename T>
-auto base_t<T>::children_fwd() const{
+constexpr auto base_t<T>::children_fwd() const{
 
     struct self{
         node_iterator begin() const {return (*base.children()).first;}
@@ -419,7 +576,7 @@ auto base_t<T>::children_fwd() const{
 }
 
 template <typename T>
-auto base_t<T>::attrs_fwd() const{
+constexpr auto base_t<T>::attrs_fwd() const{
 
     struct self{
         attr_iterator begin() const {return (*base.attrs()).first;}
@@ -447,7 +604,7 @@ struct Tree{
      * @param fn Criterion to determine the order of nodes. -1 for left<right, 0 for equals, 1 for left>right
      * @return true if the operation is successful
      */
-    bool reorder_children(const node_t* ref, const std::function<int(const unknown_t*, const unknown_t*)>& fn){}
+    bool reorder_children(const node_t* ref, const std::function<int(const unknown_t*, const unknown_t*)>& fn){return false;}
 
     /**
      * @brief Reorder (in-place) attributes of a node based on a custom ordering criterion.
@@ -456,7 +613,7 @@ struct Tree{
      * @param fn Criterion to determine the order of nodes. -1 for left<right, 0 for equals, 1 for left>right
      * @return true if the operation is successful
      */
-    bool reorder_attrs(const node_t* ref, const std::function<int(const attr_t*, const attr_t*)>& fn){}
+    bool reorder_attrs(const node_t* ref, const std::function<int(const attr_t*, const attr_t*)>& fn){return false;}
 
     /**
      * @brief 
@@ -492,14 +649,22 @@ struct Tree{
             if(ptr->children()->first==ptr->children()->second){
                 out << std::format("<{}{}{}", *ptr->ns(), ptr->ns()==""?"":":", *ptr->name());
                 for(auto& i : ptr->attrs_fwd()){
-                    out << std::format(" {}{}{}=\"{}\"", *i.ns(), *i.ns()==""?"":":", *i.name(), *i.value());   //TODO: escape value
+                    auto t = serialize::to_xml_attr_2(*i.value());
+                    if(!t.has_value()){/*TODO: Error*/}
+                    auto tt = t.value_or(std::string_view(""));
+                    std::string_view sv = std::holds_alternative<std::string>(tt)?std::get<std::string>(tt):std::get<std::string_view>(tt);
+                    out << std::format(" {}{}{}=\"{}\"", *i.ns(), *i.ns()==""?"":":", *i.name(), sv);
                 }
-                out << ">";
+                out << "/>";
             }
             else{
                 out << std::format("<{}{}{}", *ptr->ns(), ptr->ns()==""?"":":", *ptr->name());
                 for(auto& i : ptr->attrs_fwd()){
-                    out << std::format(" {}{}{}=\"{}\"", *i.ns(), *i.ns()==""?"":":", *i.name(), *i.value());   //TODO: escape value
+                    auto t = serialize::to_xml_attr_2(*i.value());
+                    if(!t.has_value()){/*TODO: Error*/}
+                    auto tt = t.value_or(std::string_view(""));
+                    std::string_view sv = std::holds_alternative<std::string>(tt)?std::get<std::string>(tt):std::get<std::string_view>(tt);
+                    out << std::format(" {}{}{}=\"{}\"", *i.ns(), *i.ns()==""?"":":", *i.name(), sv);
                 }
                 out << ">";
                 for(auto& i : ptr->children_fwd()){
@@ -508,7 +673,37 @@ struct Tree{
                 out << std::format("</{}{}{}>", *ptr->ns(), ptr->ns()==""?"":":", *ptr->name());
             }
         }
-        
+        else if(ptr->type()==type_t::CDATA){
+            auto t = serialize::to_xml_cdata(*ptr->value());
+            if(!t.has_value()){/*TODO: Error*/}
+            auto tt = t.value_or(std::string_view(""));
+            std::string_view sv = std::holds_alternative<std::string>(tt)?std::get<std::string>(tt):std::get<std::string_view>(tt);
+            out << std::format("<![CDATA[{}]]>",sv);
+        }
+        else if(ptr->type()==type_t::COMMENT){
+            auto t = serialize::to_xml_comment(*ptr->value());
+            if(!t.has_value()){/*TODO: Error*/}
+            auto tt = t.value_or(std::string_view(""));
+            std::string_view sv = std::holds_alternative<std::string>(tt)?std::get<std::string>(tt):std::get<std::string_view>(tt);
+            out << std::format("<!--{}-->",sv);
+        }
+        else if(ptr->type()==type_t::TEXT){
+            auto t = serialize::to_xml_text(*ptr->value());
+            if(!t.has_value()){/*TODO: Error*/}
+            auto tt = t.value_or(std::string_view(""));
+            std::string_view sv = std::holds_alternative<std::string>(tt)?std::get<std::string>(tt):std::get<std::string_view>(tt);
+            out << std::format("{}",sv);
+        }
+        else if(ptr->type()==type_t::PROC){
+            auto t = serialize::to_xml_proc(*ptr->value());
+            if(!t.has_value()){/*TODO: Error*/}
+            auto tt = t.value_or(std::string_view(""));
+            std::string_view sv = std::holds_alternative<std::string>(tt)?std::get<std::string>(tt):std::get<std::string_view>(tt);
+            out << std::format("<?{}?>",sv);
+        }
+        else if(ptr->type()==type_t::INJECT){
+            //Skip, injection points are not XML, they are only internally used.
+        }
         return false;
     };
 };
@@ -528,6 +723,29 @@ struct Builder{
     bool attribute_block = false;   //True after a begin to add attributes. It is automatically closed when any other command is triggered.
 
     std::stack<std::pair<ptrdiff_t,ptrdiff_t>> stack;
+
+    template<typename T>
+    error_t leaf(std::string_view value){
+        if(open==false)return error_t::TREE_CLOSED;
+        attribute_block=false;
+
+        buffer.resize(buffer.size()+sizeof(T));
+
+        auto& old_ctx = stack.top();
+
+        unknown_t* prev = old_ctx.second!=-1?(unknown_t*)(buffer.data()+old_ctx.second):nullptr;
+
+        T* tmp_node = new ((node_t*) & (uint8_t&) *( buffer.end()-sizeof(T) )) T(value);
+
+        if(prev!=nullptr){
+            prev->set_next((unknown_t*)tmp_node);
+            tmp_node->set_prev(prev);
+        }
+        old_ctx.second = (uint8_t*)tmp_node-buffer.data();
+
+        return error_t::OK;
+    }
+
 
     public:
 
@@ -586,18 +804,19 @@ struct Builder{
         node_t* parent = (node_t*)(buffer.data()+old_ctx.first);
 
         //Emplace node
-        attr_t* tmp_attr = new ((attr_t*) & (uint8_t&) *( buffer.end()-sizeof(attr_t) )) attr_t(ns,name,value);
+        new ((attr_t*) & (uint8_t&) *( buffer.end()-sizeof(attr_t) )) attr_t(ns,name,value);
 
         parent->attrs_count++;
 
         return error_t::OK;
     }
 
-    error_t text(std::string_view value);
-    error_t comment(std::string_view value){}
-    error_t cdata(std::string_view value){}
-    error_t proc(std::string_view value){}
-    error_t inject(std::string_view value){}
+ 
+    inline error_t text(std::string_view value){return leaf<text_t>(value);}
+    inline error_t comment(std::string_view value){return leaf<comment_t>(value);}
+    inline error_t cdata(std::string_view value){return leaf<cdata_t>(value);}
+    inline error_t proc(std::string_view value){return leaf<proc_t>(value);}
+    inline error_t inject(std::string_view value){return leaf<inject_t>(value);}
 
     std::expected<Tree,error_t> close(){
         if(open==false)return std::unexpected(error_t::TREE_CLOSED);
@@ -614,35 +833,22 @@ struct Parser {};
 
 
 int main(){
-    /*
-    //hello();
-    xml::base_t<xml::comment_t> cmt;
-    cmt.ns();
-
-    xml::base_t<xml::node_t> hellp;
-    auto w = hellp.ns();
-
-    const xml::unknown_t& ww = (const xml::unknown_t&)hellp;
-    ww.ns();
-    std::print("{}",*hellp.ns());
-    */
-
     xml::Builder build;
     build.begin("hello");
-        build.attr("op1", "val1");
+        build.attr("op1", "v'>&al1");
         build.attr("op2", "val1", "w");
-        build.attr("op3", "val1");
+        build.attr("op3", "val\"1");
         build.begin("hello1","s");
-            
         build.end();
         build.begin("hello2","s");
-        
+            build.text("Banana <hello ciao=\"worldo\" &amp; &></world>"); 
         build.end();
         build.begin("hello3","s");
             build.begin("hello5","s");
             build.attr("op1", "val1");
             build.attr("op2", "val1");
             build.attr("op3", "val1");
+            build.cdata("Hello'''''&&&& world!");
             build.end();
         build.end();
         build.begin("hello4","s");
@@ -657,19 +863,20 @@ int main(){
         std::print("{}, {}\n",(long)i,(long)limits.second);
         std::print("{} \n",*i->name());
         if(!i->next().has_value())break;
-    }*/
+    }
     for(auto& i : tree.root.attrs_fwd()){
         std::print("{} \n",*i.name());
     }
     for(auto& i : tree.root.children_fwd()){
         std::print("{} \n",*i.name());
         for(auto& j : i.children_fwd()){
-            std::print("{} \n",*j.name());
+            //std::print("{} \n",*j.name());
         }
     }
+    */
+    std::print("{},{},{}\n",sizeof(std::variant<std::string,std::string_view>),sizeof(std::string),sizeof(std::string_view));
 
     tree.print(std::cout,{});
-    //node_t vv;
-    //auto q = vv.get<node_t>();
+
     return 0;
 }
