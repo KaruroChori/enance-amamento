b52e8d91eca10f07eb731e32b983c842682b9bb1
Karuro Chari <karurochori@users.noreply.github.com>
2025-06-14 18:42:02 +0100

    Improved documentation generation by enabling mermaid extension in markdown.

--- src/gen-schemas/helper.ts
+++ src/gen-schemas/helper.ts
@@ -1,1 +1,5 @@
 #!/bin/env bun
+
+//Glob `include/enamento/sdf/**`
+//Match `struct *{` in files.
+//Generate a function applying a function to all of them (after ordering?).
--- src/gen-schemas/main.cpp
+++ src/gen-schemas/main.cpp
@@ -1,3 +1,22 @@
+#include <fstream>
+#include <vs-xml/builder.hpp>
+
+#if __has_include("entries.autogen.hpp")
+    #include "entries.autogen.hpp"
+    //TODO: Check for library version to match the one of the entries. Just to ensure that when releasing this index gets updated first.
+#else 
+    #error "You will have to run `helper.ts` before compiling this application"
+#endif
+
 int main(){
+    std::ofstream schema_file("./dist/root-website/schema.xml");
+    xml::Builder schema_builder;
+
+    //TODO: build in here
+
+    auto schema_xml = *schema_builder.close();
+    schema_xml.print(schema_file,{});
+
+    schema_file.close();
     return 0;
 }
